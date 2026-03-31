7bc8887357b11c3837a25f15f87d011c395af5fc
Chinmay-Raj-Git <ychinmayraj06@gmail.com>
2026-03-31 23:48:43 +0530

    Updated Lavender color from COLOR HEX

--- sniipe-react-frontend/src/components/ColorSelector.jsx
+++ sniipe-react-frontend/src/components/ColorSelector.jsx
@@ -7,13 +7,13 @@ import { useTheme } from '../theme/ThemeContext'
 const COLOR_HEX = {
   black: '#1c1c1c', white: '#f5f5f5', red: '#d63031',
   blue: '#0984e3', navy: '#1a365d', green: '#ADDE7A',
-  forest_green: '#224C32',
+  forest_green: '#224C32', lavender: '#d6befa',
   yellow: '#fdcb6e', orange: '#e17055', purple: '#a29bfe',
   pink: '#fd79a8', grey: '#636e72', gray: '#636e72',
   brown: '#c19a6b', dark_brown: '#8B4513', beige: '#d6cbb5', cream: '#fffdd0',
   maroon: '#800000', teal: '#008080', olive: '#808000',
   khaki: '#c3b091', charcoal: '#36454f', coral: '#ff7f7f',
-  mint: '#98ff98', lavender: '#e6e6fa', tan: '#d2b48c',
+  mint: '#98ff98', cool_lavender: '#e6e6fa', tan: '#d2b48c',
   camel: '#c19a6b', ivory: '#fffff0', sand: '#c2b280',
 }
 
